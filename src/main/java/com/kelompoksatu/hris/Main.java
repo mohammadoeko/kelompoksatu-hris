0822db94015aa09781fd84bd937e383a70a3fdd5
mohammadoeko <mohammadoeko@gmail.com>
2022-11-10 21:40:14 +0700

    test push

--- src/main/java/com/kelompoksatu/hris/Main.java
+++ src/main/java/com/kelompoksatu/hris/Main.java
@@ -3,5 +3,6 @@ package com.kelompoksatu.hris;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Hello world!");
     }
 }
